529323c10356c5001c75cb5a59a2d96c4c90a9b5
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2025-11-16 13:55:31 +0600

    Added Registration form

---
+++ MID_TERM/LAB_TASK-2/index.js
@@ -0,0 +1,41 @@
+function handleSubmit() {
+    // Get values from form
+    var name = document.getElementById("name").value.trim();
+    var email = document.getElementById("studentEmail").value.trim();
+    var phoneNumber = document.getElementById("phoneNumber").value.trim();
+    var password = document.getElementById("password").value.trim();
+    var confirmPassword = document.getElementById("confirmPassword").value.trim();
+
+    var errorDiv = document.getElementById("error");
+    var outputDiv = document.getElementById("output");
+
+    // Clear previous messages
+    errorDiv.innerHTML = "";
+    outputDiv.innerHTML = "";
+
+    // Validation
+    if (name === "" || email === "" || phoneNumber === "" || password === "" || confirmPassword ==="") {
+        errorDiv.innerHTML = "Please fill in all fields.";
+        return false;
+    }
+
+    if (!email.includes("@")) {
+        errorDiv.innerHTML = " Email must contain @.";
+        return false;
+    }
+
+    if (isNaN(phoneNumber) || phoneNumber === "") {
+        errorDiv.innerHTML = "Phone number must contain only digits";
+        return false;
+    }
+
+
+    outputDiv.innerHTML = `
+        <strong>Registration Successful!</strong><br><br>
+        Name: ${name}<br>
+        Email: ${email}<br>
+        Phone: ${phoneNumber}<br>
+      `;
+
+    return false;
+}
