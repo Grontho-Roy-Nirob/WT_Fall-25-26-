e5c29135bcbc5ff17c370c93a59d1a0e0ac74310
Grontho Roy <gronthoroy2023@gmail.com>
2026-01-05 00:31:23 +0600

    Delete PROJECT/USER/MVC directory

--- PROJECT/USER/MVC/Javascript/dashboard.js
+++
@@ -1,6 +0,0 @@
-const buyNowBtn = document.getElementById('buyNowBtn');
-
-function goToBooksPage() {
-    window.location.href = '../php/books.php'; 
-}
-buyNowBtn.addEventListener('click', goToBooksPage);
